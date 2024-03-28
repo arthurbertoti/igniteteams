929fd7b82a918837dcbe27086a8cb219c4a500a6
Arthur Bertoti <artgbertoti@gmail.com>
2024-03-28 18:29:07 -0300

    feat(Groups): add FlatList

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,19 @@
+import { useState } from "react"
+import { FlatList } from "react-native"
 import { Container } from "./styles"
 import { GroupCard, Header, Highlight } from "@components/index"
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galera do Ignite"])
   return (
     <Container>
       <Header showBackIcon />
       <Highlight title={"Turma"} subtitle={"Jogue com a sua turma"} />
-      <GroupCard title={"Galera do Ignite"} />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   )
 }
