86aa6c37d343a084bad99b585b7351930e831929
Arthur Bertoti <artgbertoti@gmail.com>
2024-03-28 15:20:28 -0300

    feat(Header): add BackButton option

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,19 @@
-import { Container, Logo } from "./styles"
+import { BackButton, BackIcon, Container, Logo } from "./styles"
 
 import LogoImg from "@assets/logo.png"
 
-export function Header() {
+type HeaderProps = {
+  showBackIcon?: boolean
+}
+
+export function Header({ showBackIcon = false }: HeaderProps) {
   return (
     <Container>
+      {showBackIcon && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
       <Logo source={LogoImg} />
     </Container>
   )
