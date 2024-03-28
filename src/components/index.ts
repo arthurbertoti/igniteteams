55d0ad89f8400abc9ee4d8431ecf444d0472eb7c
Arthur Bertoti <artgbertoti@gmail.com>
2024-03-28 16:38:50 -0300

    feat(components): create and use GroupCard

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./Header"
 export * from "./Loading"
 export * from "./Highlight"
+export * from "./GroupCard"
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,12 @@
 import { Container } from "./styles"
-import { Header, Highlight } from "@components/index"
+import { GroupCard, Header, Highlight } from "@components/index"
 
 export function Groups() {
   return (
     <Container>
       <Header showBackIcon />
       <Highlight title={"Turma"} subtitle={"Jogue com a sua turma"} />
+      <GroupCard title={"Galera do Ignite"} />
     </Container>
   )
 }
