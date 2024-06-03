18c99ec834faf1e839606be57f2219b7544d8d8f
Arthur Bertoti <artgbertoti@gmail.com>
2024-06-03 15:49:00 -0300

    feat(storage): add create and read groups function

---
+++ src/storage/group/groupCreate.ts
@@ -0,0 +1,13 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { GROUP_COLLECTION } from "@storage/storageConfig"
+import { groupsGetAll } from "./groupsGetAll"
+
+export async function groupCreate(newGroup: string) {
+  try {
+    const storedGroups = await groupsGetAll()
+    const updatedGroups = JSON.stringify([...storedGroups, newGroup])
+    await AsyncStorage.setItem(GROUP_COLLECTION, updatedGroups)
+  } catch (error) {
+    throw error
+  }
+}
