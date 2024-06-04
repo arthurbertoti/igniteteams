4addfc96bcd59c5afa74e125327d3eb28ff5b6ce
Arthur Bertoti <artgbertoti@gmail.com>
2024-06-04 11:17:52 -0300

    feat(Players): use playerGetByGroup function

--- src/screens/Player/index.tsx
+++ src/screens/Player/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { FlatList } from "react-native"
+import { Alert, FlatList } from "react-native"
 import { useRoute } from "@react-navigation/native"
 
 import {
@@ -13,12 +13,17 @@ import {
   PlayerCard,
 } from "@components/index"
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles"
+import { PlayerStorageDTO } from "@storage/player/playerStorageDTO"
+import { AppError } from "@utils/AppError"
+import { playerAddByGroup } from "@storage/player/playerAddByGroup"
+import { playerGetByGroup } from "@storage/player/playerGetByGroup"
 
 type RouteParams = {
   group: string
 }
 
 export function Players() {
+  const [newPlayerName, setNewPlayerName] = useState("")
   const [team, setTeam] = useState("Time A")
   const [players, setPlayers] = useState([])
 
@@ -26,13 +31,43 @@ export function Players() {
 
   const { group } = route.params as RouteParams
 
+  async function handleAddPlayer() {
+    if (newPlayerName.trim().length === 0) {
+      return Alert.alert(
+        "Nova pessoa",
+        "informe o nome da pessoa para adicionar"
+      )
+    }
+    const newPlayer: PlayerStorageDTO = {
+      name: newPlayerName,
+      team,
+    }
+    try {
+      await playerAddByGroup(newPlayer, group)
+      const players = await playerGetByGroup(group)
+      console.log(players)
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert("Nova pessoa", error.message)
+      } else {
+        console.log(error)
+        Alert.alert("Nova pessoa", "Não foi possível adicionar.")
+      }
+    }
+  }
+
   return (
     <Container>
       <Header showBackIcon />
       <Highlight title={group} subtitle="adicione a galera e separe os times" />
       <Form>
-        <Input placeholder="Nome da pessoa" autoCorrect={false} />
-        <ButtonIcon iconName="add" type={"PRIMARY"} />
+        <Input
+          onChangeText={setNewPlayerName}
+          value={newPlayerName}
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
+        <ButtonIcon onPress={handleAddPlayer} iconName="add" type={"PRIMARY"} />
       </Form>
       <HeaderList>
         <FlatList
